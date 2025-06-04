36f36a05dbde1a7bf1b489d67d9e234b4a812be9
gbp_xl - (Gabriel Paiola) <paiola.gabriel@escola.pr.gov.br>
2025-06-04 10:43:12 -0300

    f

--- perguntas.js
+++ perguntas.js
@@ -7,29 +7,29 @@ criaCartao(
 criaCartao(
     'EB - Exército Brasileiro',
     'Dentro do ambitô militar, pode se usar abreviações?',
-    'Não, inclusive a quebra desta regra, resunta em punições.'
+    'Não.'
 )
 
 criaCartao(
     'BPE - Batalhão de Polícia do Exército',
     'Quem é responsável pela unidade?',
-    'Normalmente e o Comandante ou SubComandante.'
+    'Comandante ou SubComandante.'
 )
 
 criaCartao(
     'ESA - Escola de Sargentos das Armas',
     'Quais baterias você pode encontrar na ESA?',
-    'Normalmento você pode encontrar de 12 a 15.'
+    '15 a 12'
 )
 
 criaCartao(
     'SGEx - Secretaria Geral do Exército',
     'Qual é o cargo máximo?',
-    'O cargo máximo é o Secretário Geral.'
+    'Secretário Geral.'
 )
 
 criaCartao(
     'CIOU - Centro de Instruções de Operações Urbanas',
     'Qual função que desempenha as aréa de Surpervisão?',
-    'Os responsáveis são o [Coor] Coordenador, e [Sup] Supervisor.'
+    '[Coor] Coordenador, e [Sup] Supervisor.'
 )
